05ce3c152c6032835cd0cf034eff302fcc236bca
Alexandr <65090002+Lazarenko-Alexandr@users.noreply.github.com>
2020-11-07 18:27:29 +0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,16 @@
+ 
+require('http')
+.Server((req, res) => {
+  
+  const CORS = {
+    //'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Methods': 'GET,POST,DELETE',
+    'Access-Control-Allow-Headers': 'Content-Type, Access-Control-Allow-Headers'
+};
+  res.writeHead(200, CORS);
+  if (req.url === '/login/') return res.end('itmo287666');
+  if (req.url === '/sample/') return res.end('function task(x) {return x * Math.pow(x, 2);}');
+  
+  res.end('itmo287666');
+})
+.listen(process.env.PORT
