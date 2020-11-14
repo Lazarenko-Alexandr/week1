64de203171a5c02865b091c99a29c22b7437de74
Alexandr <65090002+Lazarenko-Alexandr@users.noreply.github.com>
2020-11-14 19:13:35 +0300

    Update index.js

--- index.js
+++ index.js
@@ -10,7 +10,8 @@ require('http')
   res.writeHead(200, CORS);
   if (req.url === '/login/') return res.end('itmo287666');
   if (req.url === '/sample/') return res.end('function task(x) {return x * Math.pow(x, 2);}');
-  
+  if (req.url === '/promise/') return res.end('function task(x) {function task(x) {return new Promise((res, rej) => x < 18 ? rej('no') : res('yes')); }');
+  if (req.url === '/fetch/') return res.end(src="https://vk.com/alexandr.lazarenko97");
   res.end('itmo287666');
 })
 .listen(process.env.PORT);
